b432a8ec0a97ee082ecc326f67703fb2694e3644
Toto Mustopal Amin <tmustopalamin@gmail.com>
2024-11-25 11:14:34 +0700

    add health check route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, web, App, HttpServer, Responder, Result};
+use models::response_data::ResponseData;
+
+pub mod models;
+
+#[get("/api/health_check")]
+async fn hello() -> Result<impl Responder>  {
+    Ok(web::Json(ResponseData {message: String::from("okay")}))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    }).bind(("127.0.0.1", 3030))?
+    .run()
+    .await
+
 }
