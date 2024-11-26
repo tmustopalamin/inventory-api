5c343341b139cbe201150efafaafb19a4bd213d2
Toto Mustopal Amin <tmustopalamin@gmail.com>
2024-11-26 11:29:25 +0700

    try to add error handling with context

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,60 @@
-use actix_web::{get, web, App, HttpServer, Responder, Result};
-use models::response_data::ResponseDataSuccess;
+use actix_web::{error, get, http::{header::ContentType, StatusCode}, web, App, HttpResponse, HttpServer, Responder, Result};
+use derive_more::derive::{Display, Error};
 
+use models::response_data::{ResponseDataError, ResponseDataSuccess};
 
 pub mod schema;
 pub mod models;
 pub mod routes;
 pub mod utils;
+
+#[derive(Debug, Display, Error)]
+enum MyError {
+    InternalError,    
+    Timeout,
+
+    #[display("not found")]
+    NotFound { field: String, value: String },
+
+    #[display("bad client data")]
+    BadClientData { field: String, value: String },
+}
+
+impl error::ResponseError for MyError {
+    fn error_response(&self) -> HttpResponse {
+        let err_response = match self {
+            MyError::InternalError => ResponseDataError {
+                code: "500".to_string(),
+                message: "terjadi kesalahan internal".to_string(),
+            },
+            MyError::BadClientData { field, value } => ResponseDataError {
+                code: "400".to_string(),
+                message: format!("field={} dan value={}", field, value),
+            },
+            MyError::Timeout => ResponseDataError {
+                code: "504".to_string(),
+                message: "waktu habis".to_string(),
+            },
+            MyError::NotFound { field, value } => ResponseDataError {
+                code: "404".to_string(),
+                message: format!("field={} dan value={} tidak ditemukan", field, value),
+            },
+        };
+
+        HttpResponse::build(self.status_code())
+            .insert_header(ContentType::json())
+            .json(err_response)
+    }
+
+    fn status_code(&self) -> StatusCode {
+        match self {
+            MyError::InternalError => StatusCode::INTERNAL_SERVER_ERROR,
+            MyError::BadClientData{ .. } => StatusCode::BAD_REQUEST,
+            MyError::Timeout => StatusCode::GATEWAY_TIMEOUT,
+            MyError::NotFound{ .. } => StatusCode::NOT_FOUND,
+        }
+    }
+}
 
 #[get("/api/health_check")]
 async fn hello() -> Result<impl Responder>  {
@@ -23,6 +72,7 @@ async fn main() -> std::io::Result<()> {
             .service(routes::item_route::get_items)
             .service(routes::item_route::get_item)
             .service(routes::item_route::insert_item)
+            .service(routes::item_route::update_item)
     }).bind(("127.0.0.1", 3030))?
     .run()
     .await
--- src/routes/item_route.rs
+++ src/routes/item_route.rs
@@ -1,11 +1,11 @@
-use actix_web::{get, post, web, HttpResponse, Responder, Result};
+use actix_web::{get, post, put, web, HttpResponse, Responder, Result};
 
 use diesel::prelude::*;
 
-use crate::{models::{item::{Item, ItemDto}, response_data::{ResponseDataError, ResponseDataSuccess}}, schema::items, utils::db::establish_connection};
+use crate::{models::{item::{Item, ItemDto}, response_data::{ResponseDataError, ResponseDataSuccess}}, schema::items, utils::db::establish_connection, MyError};
 
 #[get("/api/items")]
-async fn get_items() -> Result<impl Responder>  {
+async fn get_items() -> Result<HttpResponse>  {
     
     use crate::schema::items::dsl::*;
 
@@ -16,24 +16,34 @@ async fn get_items() -> Result<impl Responder>  {
         .expect("Error loading posts");
 
 
-    Ok(web::Json(results))
+    Ok(HttpResponse::Ok().json(results))
 }
 
 #[get("/api/items/{id}")]
-async fn get_item(path: web::Path<i32>) -> Result<impl Responder>  {
+async fn get_item(path: web::Path<i32>) -> Result<HttpResponse, MyError>  {
     let id_item = path.into_inner();
 
     use crate::schema::items::dsl::*;
 
     let connection = &mut establish_connection();
-    let results = items
-        .filter(id.eq(id_item))
-        .select(Item::as_select())
-        .first(connection)
-        .expect("Error loading posts");
-
-
-    Ok(web::Json(results))
+    let results = match items
+        .filter(id.eq(id_item)) 
+        .select(Item::as_select()) 
+        .first(connection) {
+            Ok(data) => data,
+            Err(err) => {
+                match err {
+                    diesel::result::Error::NotFound => {
+                        return Err(MyError::NotFound { field: "id".to_string(), value: id_item.to_string() } );
+                    }
+                    _ => {
+                        return Err(MyError::InternalError );
+                    }
+                }
+            },
+        };
+        
+    Ok(HttpResponse::Ok().json(results))
 }
 
 #[post("/api/items")]
@@ -60,6 +70,33 @@ async fn insert_item(body_data: web::Json<ItemDto>) -> Result<impl Responder>  {
 
     return Ok(HttpResponse::Ok().json(ResponseDataSuccess::<usize> {
         message: "insert success".to_string(),
+        data: None
+    }));
+}
+
+#[put("/api/items/{id}")]
+async fn update_item(path: web::Path<i32>, body_data: web::Json<ItemDto>) -> Result<impl Responder>  {
+    use crate::schema::items::dsl::*;
+    
+    let id_item = path.into_inner();
+
+    let updated_name = body_data.name.clone().unwrap_or("".to_string());
+    if updated_name.is_empty() {
+        let err = ResponseDataError {
+            code: "empty_field".to_string(),
+            message: "kolom nama perlu di isi".to_string()
+        };
+        return Ok(HttpResponse::BadRequest().json(err));
+    }
+
+    let connection = &mut establish_connection();
+    diesel::update(items.find(id_item))
+        .set(name.eq(updated_name))
+        .execute(connection)
+        .expect("Error saving new post");
+
+    return Ok(HttpResponse::Ok().json(ResponseDataSuccess::<usize> {
+        message: "update success".to_string(),
         data: None
     }));
 }
